2a3f3d0917fe9cfd710cf1d708c010f1e68deab9
Daniel <danielhazzer2009@gmail.com>
2025-08-01 17:40:52 -0300

    modal noticias

--- js/global.js
+++ js/global.js
@@ -47,15 +47,60 @@ const numeroWpp = "5491165835895";
 const mensaje = "Hola, quiero hacer un reclamo sobre mi compra.";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const wppLink = document.querySelector(".link-atencion");
-  if (wppLink) {
-    wppLink.href = `https://wa.me/${numeroWpp}?text=${encodeURIComponent(mensaje)}`;
-  }
+    const wppLink = document.querySelector(".link-atencion");
+    if (wppLink) {
+        wppLink.href = `https://wa.me/${numeroWpp}?text=${encodeURIComponent(mensaje)}`;
+    }
 
-  // Actualizar año automáticamente
-  const yearElement = document.querySelector(".copyright");
-  if (yearElement) {
-    const year = new Date().getFullYear();
-    yearElement.innerHTML = `Copyright Import Tech · ${year}. Todos los derechos reservados.`;
-  }
+    // Actualizar año automáticamente
+    const yearElement = document.querySelector(".copyright");
+    if (yearElement) {
+        const year = new Date().getFullYear();
+        yearElement.innerHTML = `Copyright Import Tech · ${year}. Todos los derechos reservados.`;
+    }
+});
+
+// MODAL, NOTICIAS hoy _________________________________________________________________________________________
+const overlay = document.getElementById("overlay");
+const closeBtn = document.getElementById("closeBtn");
+
+// ACTUALIZAR TITULO
+const titleModal = document.querySelector(".title-modal");
+titleModal.innerHTML = "<b>Novedades</b> · ¡Aprovechá ahora! 😉";
+// ACTUALIZAR IMAGEN PRINCIPAL
+const imgModal = document.querySelector(".modal img");
+// Obtengo la ruta actual (sin dominio)
+const rutaActual = window.location.pathname;
+// Verifico si estoy en index o en otra página
+if (rutaActual.endsWith("index.html")) {
+    // Estamos en index
+    imgModal.src = "./img/banner_3.png";
+} else {
+    // Estamos en otra página
+    imgModal.src = "../img/banner_3.png";
+}
+overlay.style.display = "block";
+
+// Función para saber si ya se mostró hoy
+// function yaSeMostroHoy() {
+//     const hoy = new Date().toDateString();
+//     return localStorage.getItem("popupFecha") === hoy;
+// }
+
+// Mostrar si no se mostró hoy
+// if (!yaSeMostroHoy()) {
+//     overlay.style.display = "block";
+//     localStorage.setItem("popupFecha", new Date().toDateString());
+// }
+
+// Cerrar modal
+closeBtn.addEventListener("click", () => {
+    overlay.style.display = "none";
+});
+
+// También cerrar si clickean fuera
+overlay.addEventListener("click", e => {
+    if (e.target === overlay) {
+        overlay.style.display = "none";
+    }
 });
